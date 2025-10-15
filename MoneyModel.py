83cf5930a7a44f9746f70b751c64d49f02845aa8
Wassila Ouerdane <wassila.ouerdane@centralesupelec.fr>
2025-10-15 23:29:01 +0200

    Implement MoneyModel and MoneyAgent classes

---
+++ MoneyModel.py
@@ -0,0 +1,82 @@
+"""a simple version of the boltman wealth model"""
+
+import mesa
+
+
+def compute_gini(model):
+    agent_wealths = [agent.wealth for agent in model.agents]
+    x = sorted(agent_wealths)
+    N = model.num_agents
+    B = sum(xi * (N - i) for i, xi in enumerate(x)) / (N * sum(x))
+    return 1 + (1 / N) - 2 * B
+
+
+class MoneyAgent(mesa.Agent):
+    """An agent with fixed initial wealth."""
+
+    def __init__(self, model):
+        """initialize a MoneyAgent instance.
+
+        Args:
+            model: A model instance
+        """
+        super().__init__(model)
+        self.wealth = 1
+
+    def move(self):
+        """move to a random neighboring cell."""
+        possible_steps = self.model.grid.get_neighborhood(
+            self.pos, moore=True, include_center=False
+        )
+        new_position = self.random.choice(possible_steps)
+        self.model.grid.move_agent(self, new_position)
+
+    def give_money(self):
+        """give money to another agent in the same gridcell."""
+        cellmates = self.model.grid.get_cell_list_contents([self.pos])
+        if len(cellmates) > 1:
+            other = self.random.choice(cellmates)
+            other.wealth += 1
+            self.wealth -= 1
+
+    def step(self):
+        """do one step of the agent."""
+        self.move()
+        if self.wealth > 0:
+            self.give_money()
+
+
+class MoneyModel(mesa.Model):
+    """A model with some number of agents."""
+
+    def __init__(self, n=10, width=10, height=10, seed=None):
+        """Initialize a MoneyModel instance.
+
+        Args:
+            N: The number of agents.
+            width: width of the grid.
+            height: Height of the grid.
+        """
+        super().__init__(seed=seed)
+        self.num_agents = n
+        self.grid = mesa.space.MultiGrid(width, height, True)
+
+        # Create agents
+        for i in range(n):
+            agent = MoneyAgent(self)
+
+            # Add agent to random grid cell
+            x = self.random.randrange(self.grid.width)
+            y = self.random.randrange(self.grid.height)
+            self.grid.place_agent(agent, (x, y))
+
+
+        self.datacollector = mesa.DataCollector(
+            model_reporters={"Gini": compute_gini}, agent_reporters={"Wealth": "wealth"}
+        )
+        self.datacollector.collect(self)
+
+    def step(self):
+        """do one step of the model"""
+        self.agents.shuffle_do("step")
+        self.datacollector.collect(self)
